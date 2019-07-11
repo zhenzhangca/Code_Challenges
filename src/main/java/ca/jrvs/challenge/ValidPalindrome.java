7a5bef6b66d47942eac4affd620af276c2674d38
Zhen.Zhang <zhen.zhang.jrvs.ca@gmail.com>
2019-07-11 18:31:04 +0000

    update code_challange

---
+++ src/main/java/ca/jrvs/challenge/ValidPalindrome.java
@@ -0,0 +1,20 @@
+package ca.jrvs.challenge;
+
+public class ValidPalindrome {
+    /**
+     * Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+     * 
+     * Note: For the purpose of this problem, we define empty string as valid palindrome.
+     *
+     * Input: "A man, a plan, a canal: Panama"
+     *
+     * Output: true
+     *
+     * Input: "race a car"
+     *
+     * Output: false
+     */
+    public boolean isPalindrome(String s) {
+        return false;
+    }
+}
