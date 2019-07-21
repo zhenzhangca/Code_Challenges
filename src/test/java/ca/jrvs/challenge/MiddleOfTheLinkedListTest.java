0aa8db263cf59a1a16f019d1a36897c1d7881027
ZhenZhang <zhen.zhang.jrvs.ca@gmail.com>
2019-07-21 23:19:18 +0000

    update code_challange

---
+++ src/test/java/ca/jrvs/challenge/MiddleOfTheLinkedListTest.java
@@ -0,0 +1,14 @@
+package ca.jrvs.challenge;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MiddleOfTheLinkedListTest {
+
+    @Test
+    public void middleNode() {
+
+
+    }
+}
